71e53f737895dddf449f554b09fb8ec9818fdf64
Jeffiros <krittiwat.c@gmail.com>
2024-10-22 05:15:26 +0700

    Add project files.

---
+++ Tourist Attraction/Models/Trip.cs
@@ -0,0 +1,12 @@
+﻿namespace Tourist_Attraction.Models
+{
+    public class Trip
+    {
+        public string Title { get; set; }
+        public string Eid { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public List<string> Photos { get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
